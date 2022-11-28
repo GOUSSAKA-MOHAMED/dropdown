2489ca75e4549ca6f4f34bdfc04a8985136645d3
GOUSSAKA MED <josh@joshs-MacBook-Pro.local>
2022-11-28 09:42:50 +0100

    drop-down

---
+++ main.js
@@ -0,0 +1,70 @@
+let inp = document.getElementById('inp');
+let btn =document.getElementById('btn');
+let lst =document.querySelectorAll('.box');
+let drag = null;
+
+
+btn.onclick = function (){
+    if (inp.value != '') {
+        lst[0].innerHTML += `
+        <p class = "item" draggable = "true">${inp.value } </p>`
+        inp.value = '';
+    }
+    dragItem();
+}
+
+
+
+function  dragItem() {
+let items = document.querySelectorAll('.item');
+items.forEach(item => {
+    item.addEventListener('dragstart',function () {
+        console.log('drag start');
+            drag = item; 
+        item.style.opacity = "0.5";
+    })
+
+    item.addEventListener('dragend',function () {
+        console.log('drag end');
+        drag = null; 
+        item.style.opacity = "1"
+    })
+
+    lst.forEach(box => {
+        box.addEventListener('dragover',function(e) {
+            e.preventDefault();
+            this.style.background = '#090';
+            this.style.color = '#fff';
+    }) 
+
+    box.addEventListener('dragleave',function () {
+        this.style.background = '#fff';
+        this.style.color = '#000';
+    }) 
+    box.addEventListener('drop',function(){
+        box.append(drag);
+        this.style.background = '#fff';
+        this.style.color = '#090';
+    })
+
+    })
+}) 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
